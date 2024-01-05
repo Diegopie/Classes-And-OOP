c300747ec21a2b9bc40d37e2894692dbf3dca42a
Diegopie <ishtaracademy@outlook.com>
2024-01-04 17:46:23 -0700

    clearing out filled in components

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import { useEffect, useState } from 'react'
-import { NintendoSwitch } from './data/Switch'
 import './App.css'
-import { Button, Col, Container, FormLabel, Row } from 'react-bootstrap'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 
 function App() {
 
-  const [userSwitch, setUserSwitch] = useState(null);
-
-  const [installGameValue, setInstallGameValue] = useState('');
-
-  // Make this after play games fails to load
-  const [allGames, setAllGames] = useState([]);
-  const [batteryLife, setBatteryLife] = useState(0);
-
-  const createSwitch = (color) => {
-    setUserSwitch((prevState) => {
-      prevState = new NintendoSwitch(color.toLowerCase(), [])
-        ;
-      console.log(prevState.gamesInstalled);
-      setAllGames(prevState.gamesInstalled)
-      setBatteryLife(prevState.checkBatteryLife())
-
-      return prevState;
-
-    })
-  }
+  const [userSwitch] = useState(false);
 
   return (
     <main>
@@ -38,14 +18,10 @@ function App() {
             <img className='w-50' src='https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/products/hardware/nintendo-switch-red-blue/110478-nintendo-switch-neon-blue-neon-red-front-screen-on-1200x675' />
           </Col>
           <Col>
-            <Row>
-              <Button className='mt-1 w-25' onClick={() => createSwitch('yellow')}>Yellow</Button>
-            </Row>
-            <Row>
-              <Button className='mt-1 w-25' onClick={() => createSwitch('blue')}>Blue</Button>
-            </Row>
-            <Row>
-              <Button className='mt-1 w-25' onClick={() => createSwitch('pink')}>Pink</Button>
+            <Row className='flex flex-column'>
+              <Button className='mt-1 w-25' >Yellow</Button>
+              <Button className='mt-1 w-25' >Blue</Button>
+              <Button className='mt-1 w-25' >Pink</Button>           
             </Row>
           </Col>
         </Row>
@@ -53,18 +29,15 @@ function App() {
 
       {userSwitch ?
         <Container>
-          <h2>You have a {userSwitch._color} Switch</h2>
+          <h2>You have a Switch</h2>
           <Container>
             <Row>
               <Col xs='5'>
                 <Row>
-                  Current Battery Life: {batteryLife}
+                  Current Battery Life:
                 </Row>
                 <Row>
-                  <Button variant="outline-primary" className='w-25 mt-3' onClick={() => {
-                    userSwitch.chargeSwitch();
-                    setBatteryLife(userSwitch.checkBatteryLife());
-                  }}>Charge Switch</Button>
+                  <Button variant="outline-primary" className='w-25 mt-3' >Charge Switch</Button>
                 </Row>
               </Col>
               <Col xs='6'>
@@ -72,21 +45,7 @@ function App() {
                   Play Games:
                 </Row>
                 <Container>
-                  {allGames.length > 0 ?
-                    allGames.map((game) => {
-                      return (
-                        <Row xs='3'>
-                          <Button onClick={() => {
-                            
-                            alert(`${userSwitch.playGame(game)}`)
-                            setBatteryLife(userSwitch.checkBatteryLife())
-                          }}>{game}</Button>
-                        </Row>
-                      )
-                    })
-
-                    : 'Install a Game!'
-                  }
+                  {/* TODO: Map Installed Games */}
                 </Container>
               </Col>
             </Row>
@@ -95,27 +54,10 @@ function App() {
           <Container className='mt-5'>
             <Row xs='6'>
               <label htmlFor='installGameValue'>Install A Game</label>
-              <input type='text' name='installGameValue' value={installGameValue} onChange={(e) => {
-                setInstallGameValue(e.target.value)
-              }} />
+              <input type='text' name='installGameValue' />
               <Button
                 className='m-2'
                 variant='outline-primary'
-
-                onClick={() => {
-                  console.log(installGameValue);
-                  if (installGameValue === '') {
-                    alert('Must Enter a Game!');
-                    return;
-                  } else if(userSwitch.gamesInstalled.includes(installGameValue)) {
-                    alert(`You already have ${installGameValue} installed`)
-                    return;
-                  }
-
-                  alert(userSwitch.installGame(installGameValue.trim()))
-                  setAllGames(userSwitch.gamesInstalled);
-                  setInstallGameValue('');
-                }}
               > Install Game</Button>
             </Row>
           </Container>
