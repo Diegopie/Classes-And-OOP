1d74d4709810b5defca6b3066ff361482e7612b2
Diegopie <ishtaracademy@outlook.com>
2024-01-05 20:49:51 -0700

    wapped up for now

--- src/components/Why.jsx
+++ src/components/Why.jsx
@@ -1,14 +1,19 @@
 import { useEffect, useRef, useState } from 'react'
-import { NintendoSwitch } from '../data/how'
+import { NintendoSwitch } from '../data/how.js'
+import userIcons from "../data/userImg.js";
 // import './App.css'
 import { Button, Col, Container, Row } from 'react-bootstrap'
 
 function Why() {
 
   const userSwitch = useRef(null);
+  const previousImg = useRef(null);
+
+  const [selectColor, setSelectColor] = useState('');
+
   const selectSwitchContainer = useRef(null);
 
-  const installGameValue = useRef()
+  const installGameValue = useRef();
 
   // Make this after play games fails to load
   const [allGames, setAllGames] = useState([]);
@@ -16,7 +21,14 @@ function Why() {
 
 
   const createSwitch = (color) => {
+    console.log(color);
+    if (color === '') {
+      alert('Select A Color');
+      return;
+    }
+    console.log(new NintendoSwitch(color, []));
     userSwitch.current = new NintendoSwitch(color, []);
+    console.log(userSwitch);
     setAllGames(userSwitch.current.getGamesInstalled())
     setBatteryLife(userSwitch.current.getBatteryLife())
   }
@@ -46,10 +58,44 @@ function Why() {
             <img className='w-50' src='https://assets.nintendo.com/image/upload/c_fill,w_1200/q_auto:best/f_auto/dpr_2.0/ncom/en_US/products/hardware/nintendo-switch-red-blue/110478-nintendo-switch-neon-blue-neon-red-front-screen-on-1200x675' />
           </Col>
           <Col>
-            <Row ref={selectSwitchContainer} className='flex flex-column'>
-              <Button className='mt-1 w-25' onClick={() => createSwitch('yellow')}>Yellow</Button>
-              <Button className='mt-1 w-25' onClick={() => createSwitch('blue')}>Blue</Button>
-              <Button className='mt-1 w-25' onClick={() => createSwitch('pink')}>Pink</Button>
+            <Row>
+              <h3>{selectColor}</h3>
+            </Row>
+            <Row className='Switch-Icons-Container' ref={selectSwitchContainer}>
+              {userIcons.map(image => (
+                <button className='Switch-Icon' key={image.id} onClick={(e) => {
+                  // Add Border
+                  e.target.classList.add('User-Icon-Selected');
+
+                  setSelectColor(image.color);
+
+                  // * Set Class Data
+
+                  // Removing Border:
+                  if (!previousImg.current) {
+                    previousImg.current = e.target;
+                    return
+                  }
+
+                  previousImg.current.classList.remove('User-Icon-Selected');
+
+                  previousImg.current = e.target;
+                }}>
+
+                  <img
+                    src={image.imageSrc}
+                    alt={image.color}
+                    className='Switch-Icon'
+
+                  />
+                </button>
+              ))}
+            </Row>
+            <Row>
+              {!userSwitch.current ?
+                <Button className='mt-1 w-25' onClick={() => createSwitch(selectColor)}>Get Switch!</Button>
+                : null
+              }
             </Row>
             <Row>
               {userSwitch.current ?
@@ -66,6 +112,7 @@ function Why() {
                 : null
               }
             </Row>
+
           </Col>
         </Row>
       </Container>
@@ -91,7 +138,6 @@ function Why() {
                   Play Games:
                 </Row>
                 <Container>
-                  {console.log(allGames)}
                   {allGames.length > 0 ?
 
                     allGames.map((game) => {
@@ -134,12 +180,12 @@ function Why() {
                 }
 
                 alert(userSwitch.current.installGame(installGameValue.current.value.trim()))
-              
+
                 // * The reference to the array is the same, so react to doesn't detect the change, even it is storing the value. You won't see the changes until you force react to rerender by saving or changing state somewhere else
                 // setAllGames(userSwitch.current.getGamesInstalled());
                 setAllGames([...userSwitch.current.getGamesInstalled()]);
 
- 
+
               }}>
                 <label htmlFor='installGameValue'>Install A Game</label>
                 <input type='text' name='installGameValue' ref={installGameValue} />
---
+++ src/data/userImg.js
@@ -0,0 +1,41 @@
+const profileImages = [
+  {
+      id: 1,
+      imageSrc:
+    'https://i.imgur.com/zwKJdmu.png',
+    color: 'yellow',
+  },
+  {
+      id: 2,
+      imageSrc:
+    'https://i.imgur.com/va1mKO4.png',
+    color: 'orange',
+  },
+  {
+      id: 3,
+      imageSrc:
+    'https://i.imgur.com/J3WVW32.png',
+    color: 'purple',
+  },
+  {
+      id: 4,
+      imageSrc:  
+    'https://i.imgur.com/GtlpcYn.png',
+    color: 'green',
+  },
+  {
+      id: 5,
+      imageSrc:
+    'https://i.imgur.com/p9ds8Ih.png',
+    color: 'red',
+  },
+  {
+      id: 6,
+      imageSrc:
+    'https://i.imgur.com/OHlDfXA.png',
+    color: 'blue',
+  },
+];
+
+
+export default profileImages;
